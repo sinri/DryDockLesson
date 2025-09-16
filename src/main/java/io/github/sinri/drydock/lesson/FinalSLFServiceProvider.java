afb35c5f3fe034763244876d00b2688ab289b7a1
sinri <e.joshua.s.e@gmail.com>
2025-09-16 11:26:52 +0800

    Enhance `J20` with logging for request handling and update `FinalSLFServiceProvider` with usage documentation.

--- src/main/java/io/github/sinri/drydock/lesson/FinalSLFServiceProvider.java
+++ src/main/java/io/github/sinri/drydock/lesson/FinalSLFServiceProvider.java
@@ -2,5 +2,8 @@ package io.github.sinri.drydock.lesson;
 
 import io.github.sinri.keel.logger.issue.slf4j.KeelSLF4JServiceProvider;
 
+/**
+ * {@code -Dslf4j.provider=io.github.sinri.drydock.lesson.FinalSLFServiceProvider}
+ */
 public class FinalSLFServiceProvider extends KeelSLF4JServiceProvider {
 }
--- src/main/java/io/github/sinri/drydock/lesson/zhejiang/fighter/J20.java
+++ src/main/java/io/github/sinri/drydock/lesson/zhejiang/fighter/J20.java
@@ -13,6 +13,16 @@ public class J20 extends Fighter {
 
     @Override
     protected void configureRoutes(Router router) {
-        router.route("/").handler(rc -> rc.response().end("Hello World!"));
+        router.route("/").handler(rc -> {
+            getHttpServerLogger().info("J20 is serving " + rc.request().absoluteURI());
+            rc.response().end("Hello World!")
+              .onComplete(ar -> {
+                  if (ar.failed()) {
+                      getHttpServerLogger().exception(ar.cause(), "J20 failed to serve api");
+                  } else {
+                      getHttpServerLogger().info("J20 served api successfully");
+                  }
+              });
+        });
     }
 }
